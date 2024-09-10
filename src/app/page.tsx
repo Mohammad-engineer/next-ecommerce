e7dc83896aaa38910d8a57ad8c5a1aa93b557b24
mohammad <72153665+Mohammad-engineer@users.noreply.github.com>
2024-09-10 09:10:22 +0330

    add blog and special category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import CategoryList from "@/components/CategoryList";
 import ProductList2 from "@/components/ProductList2";
 //import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
+import SpecialCategory from '../components/special-category.jsx'
 // import { WixClientContext } from "@/context/wixContext";
 // import { useContext, useEffect } from "react";
 
@@ -34,6 +35,10 @@ const HomePage = () => {
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
         <ProductList2 />
+      </div>
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <SpecialCategory />
       </div>
     
     </div>
